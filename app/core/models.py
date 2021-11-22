dbcaa2f458040280396cd38af6db1c6f3038f391
litesh1998 <litesh1998@gmail.com>
2021-11-22 22:23:53 +0530

    Added Recipe Model

--- app/core/models.py
+++ app/core/models.py
@@ -59,3 +59,21 @@ class Ingrediant(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Recipe(models.Model):
+    '''Recipe Object'''
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingregiants = models.ManyToManyField('Ingrediant')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self) -> str:
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -60,3 +60,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        '''Test the string representation of a string'''
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and Mushroom Sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
